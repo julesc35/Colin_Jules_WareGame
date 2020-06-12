836f744b82bb4b3b304c1fa418c2f103dbde073b
julesc35 <59653303+julesc35@users.noreply.github.com>
2020-06-12 16:58:04 +0200

    base ComJouer

--- Jeu/scenes/ComJouer.js
+++ Jeu/scenes/ComJouer.js
@@ -7,6 +7,15 @@ class ComJouer extends Phaser.Scene {
 
 	preload(){
 
+		// fond // 
+
+        this.add.image(400, 300, 'FondComJouer').setScale(0.30);
+
+		// boutons //
+
+
+        this.add.image(400, 300, 'BoutRetour').setScale(0.30);
+
 }
  create(){}
 
