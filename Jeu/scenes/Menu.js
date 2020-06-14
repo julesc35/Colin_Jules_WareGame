8fcd9848bab886b754c4d795016a6bb18b21aad9
julesc35 <59653303+julesc35@users.noreply.github.com>
2020-06-14 15:32:01 +0200

    interactivité menu

--- Jeu/scenes/Menu.js
+++ Jeu/scenes/Menu.js
@@ -11,21 +11,35 @@ class Menu extends Phaser.Scene {
 
                 // fond //
 
+
         this.add.image(960, 540, 'Menu').setScale(0.24);
+
 
                 // Logo // 
 
-        this.add.image(1000, 280, 'Logo').setScale(0.3).setDepth(100);          
+
+       var Logo = this.add.image(1000, 280, 'Logo').setScale(0.3).setDepth(100);          
+        Logo.setInteractive({useHandCursor: true});
+        Logo.on('pointerdown', () => this.clickLogo());
+
 
                 //boutons // 
 
-        this.add.image(1000, 600, 'BoutJouer').setScale(0.75);
 
 
-        this.add.image(1000, 775, 'BoutComJouer').setScale(0.75);
+        Bouton_Jouer = this.add.image(1000, 600, 'BoutJouer').setScale(0.75);
+        Bouton_Jouer.setInteractive({useHandCursor: true});
+        Bouton_Jouer.on('pointerdown', () => this.clickJouer());
+
+
+
+        Bouton_ComJouer = this.add.image(1000, 775, 'BoutComJouer').setScale(0.75);
+        Bouton_ComJouer.setInteractive({useHandCursor: true});
+        Bouton_ComJouer.on('pointerdown', () => this.clickComJouer());
 
 
                 // musique //
+                
 
         menuMusic = this.sound.add('menuMusic');
         menuMusic.play({volume: 0.5, loop: true});
@@ -37,3 +51,18 @@ class Menu extends Phaser.Scene {
 
  }
 }
+
+// fonctions changement de scene //
+
+/*        function clickLogo(){
+                this.scene.switch('BonusScene');
+        }
+*/
+
+        function clickJouer(){
+                this.scene.switch('MenuJeu');
+        }
+
+        function clickComJouer(){
+                this.scene.switch('ComJouer');
+        }
