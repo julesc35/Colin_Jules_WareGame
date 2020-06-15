c92bf4ec0a752e8d3764ad63ddcb393d9d97802d
julesc35 <59653303+julesc35@users.noreply.github.com>
2020-06-15 15:45:38 +0200

    .

--- Jeu/scenes/Jeu2.js
+++ Jeu/scenes/Jeu2.js
@@ -37,5 +37,25 @@ class Jeu2 extends Phaser.Scene {
 }
 
 	update(){
+		
+		// configuration des touches //
+		if (cursors.left.isDown && cursors.right.isUp){
+			player.anims.play('left', true);
+			player.setFlipX(false);
+			player.setVelocityX(-120);
+			player.setVelocityY(0);
+		}
+		else if (cursors.right.isDown && cursors.left.isUp){
+			player.anims.play('left', true);
+			player.setFlipX(true);
+			player.setVelocityX(120);
+			player.setVelocityY(0);
+		}
+		else if (cursors.right.isUp && cursors.left.isUp){
+			player.anims.play('stop', true);
+			player.setFlipX(false);
+			player.setVelocityX(0);
+			player.setVelocityY(0);
+		}
 }
 }
