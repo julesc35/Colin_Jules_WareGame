241c09540a0ac3dea2bfa7d950ab17e49e34b7a9
julesc35 <59653303+julesc35@users.noreply.github.com>
2020-06-16 15:11:53 +0200

    .

--- Jeu/scenes/Jeu1.js
+++ Jeu/scenes/Jeu1.js
@@ -55,6 +55,7 @@ class Jeu1 extends Phaser.Scene {
 
 
         this.anims.create({
+
         	key: 'left',
         	frames: this.anims.generateFrameNumbers('Pirate_Gauche', {start: 0, end: 29}),
         	frameRate: 5,
@@ -62,6 +63,7 @@ class Jeu1 extends Phaser.Scene {
         });
 
         this.anims.create({
+
         	key: 'stop',
         	frames: this.anims.generateFrameNumbers('Pirate_Face', {start: 0, end: 1}),
         	frameRate: 5,
@@ -75,18 +77,23 @@ class Jeu1 extends Phaser.Scene {
 
 
     biere = this.physics.add.group({
+
         key: 'biere',
         repeat: Phaser.Math.Between(1,5),
         setX: Phaser.Math.Between(0,1920);
     });
-    biere.setCollideWorldBounds()
+
+
     rhum = this.physics.add.group({
+
         key: 'rhum',
         repeat: Phaser.Math.Between(0,3),
         setX: Phaser.Math.Between(0,1920);
     });
 
+
     eau = this.physics.add.group({
+
         key: 'eau',
         repeat: Phaser.Math.Between(2,6),
         setX: Phaser.Math.Between(0,1920);
@@ -111,22 +118,30 @@ class Jeu1 extends Phaser.Scene {
 
 
     // debut de partie //
+
+
     this.physics.pause();
     timerdepart = this.time.addEvent({delay: 3000, callback: () =>{
+
     	text_depart1.setVisible(false);
     	this.physics.resume();
     }, 
+
     loop: false
+
 });
 
     // chrono toute la partie // 
 
     timer = this.time.addEvent({delay: 13000, callback: () =>{
+
     	victoire += 1;
     	this.physics.pause();
     	this.add.text(960, 540, 'Fin de la partie', {fontFamily: 'Treasuremap', fontSize: 130, color: '#000000'}).setDepth(10);
     },
+
     loop: false
+
 });
 
 }
@@ -141,18 +156,21 @@ class Jeu1 extends Phaser.Scene {
 
 
 		if (cursors.left.isDown && cursors.right.isUp){
+
 			player.anims.play('left', true);
 			player.setFlipX(false);
 			player.setVelocityX(-120);
 			player.setVelocityY(0);
 		}
 		else if (cursors.right.isDown && cursors.left.isUp){
+
 			player.anims.play('left', true);
 			player.setFlipX(true);
 			player.setVelocityX(120);
 			player.setVelocityY(0);
 		}
 		else if (cursors.right.isUp && cursors.left.isUp){
+
 			player.anims.play('stop', true);
 			player.setFlipX(false);
 			player.setVelocityX(0);
@@ -167,12 +185,16 @@ class Jeu1 extends Phaser.Scene {
 
 
 	function hitBiere(player, biere){
+
 		biere.disableBody(true, true);
 		score += 25;
 		score_texte.setText('X ' + score);
 		if (biere.countActive(true) <= 1){
+
 			biere.children.iterate(function(child){
+
 				child.enableBody(true, child.X, 0, true, true);
+
 			});
 		}
 
@@ -180,44 +202,62 @@ class Jeu1 extends Phaser.Scene {
 
 
 	function hitRhum(player,rhum){
+
 		rhum.disableBody(true, true);
 		score += 50;
 		score_texte.setText('X ' + score);
 		if (rhum.countActive(true) <= 1){
+
 			rhum.children.iterate(function(child){
+
 			child.enableBody(true, child.X, 0, true, true);
+
 			});
 }
 
 
 	function hitEau(player, eau){
+
 		eau.disableBody(true, true);
 		score -= 40;
 		score_texte.setText('X ' + score);
 		if (eau.countActive(true) <= 1){
+
 			eau.children.iterate(function(child){
+
 			child.enableBody(true, child.X, 0, true, true);
+
 			});
 }
 
 
 	function ExitWorld(biere, eau, rhum){
-	biere.disableBody():
-	eau.disableBody():	
-	rhum.disableBody():
+
+	biere.disableBody();
+	eau.disableBody();	
+	rhum.disableBody();
+
 }
 
 
 	function fin(){
+
 		if (victoire == 1){
+
 			this.add.text(960, 540, 'Veuillez patienter', {fontFamily: 'Treasuremap', fontSize: 130, color: '#000000'}).setDepth(10);
+
 			scoreG += score;
+
 			this.time.addEvent({delay: 5000, callback: () =>{
+
 				MusicBar.mute = true;
 				this.scene.start('Jeu2');
 			},
+
 		loop: false
+
 			});
 
 		};
+		
 	}
--- Jeu/scenes/Jeu2.js
+++ Jeu/scenes/Jeu2.js
@@ -13,69 +13,112 @@ class Jeu2 extends Phaser.Scene {
 
 	victoire = 0;
 
+		// musique //
+
+
+
+		MusicMer = this.sound.add('merMusic');
+		MusicMer.play({volume: 0.5, loop: false});
+
+		// debut //
+
+
+
+		text_depart1 = this.add.text(960, 540, 'debut dans 3 secondes !'{ fontFamily: 'Treasuremap', fontSize: 130, color: '#000000'}).setDepth(10).setVisible(false);
+
+
 		// fond // 
+
 
         this.add.image(960, 540, 'Jeu2').setScale(0.30).setDepth(1);
 
+
+
         // joueur // 
+
 
         player = this.add.sprite(960,540, 'Bateau_joueur').setDepth(2);
         player.setCollideWorldBounds(true);
 
+
         // création des animations du joueur //
 
+
         this.anims.create({
+
         	key: 'left',
         	frames: this.anims.generateFrameNumbers('Bateau_joueur', {start: 0, end: 29}),
         	frameRate: 5,
         	repeat: -1
+
         });
 
+
         this.anims.create({
+
         	key: 'stop',
         	frames: this.anims.generateFrameNumbers('Bateau_joueur', {start: 0, end: 1}),
         	frameRate: 5,
         	repeat: -1
+
         });
 
+
         // création de la vie //
+
 
         if (nbrVie == 1){ vie1 = this.add.image(1860,66, 'Vie').setDepth(3)}
         if (nbrVie == 2){ vie2 = this.add.image(1740,66, 'Vie').setDepth(3)}
         if (nbrVie == 3){ vie3 = this.add.image(1620,66, 'Vie').setDepth(3)}
 
+
         // creation des enemis //
 
 
+
     bateauEnm = this.physics.add.group({
+
         key: 'BateauEnm',
         repeat: Phaser.Math.Between(1,6),
         setX: Phaser.Math.Between(0,1920);
         setY: 0;
+
     });
 
+
     ile = this.physics.add.group({
+
         key: 'ile',
         repeat: Phaser.Math.Between(0,4),
         setX: Phaser.Math.Between(0,1920);
         setY: 0;
+
     });
 
+
     ile2 = this.physics.add.group({
+
         key: 'ile2',
         repeat: Phaser.Math.Between(0,2),
         setX: Phaser.Math.Between(0,1920);
         setY: 0;
+
     });
+
+
     rochile = this.physics.add.group({
+
         key: 'rochile',
         repeat: Phaser.Math.Between(1,4),
         setX: Phaser.Math.Between(0,1920);
         setY: 0;
+
     });
 
 
+
     // collisions //
+
 
 
 
@@ -88,6 +131,7 @@ class Jeu2 extends Phaser.Scene {
     this.physics.overlap(player, rochile, hitRochile, null, this);
 
 
+
     /////// chrono /////// 
 
 
@@ -96,44 +140,66 @@ class Jeu2 extends Phaser.Scene {
 
     this.physics.pause();
     timerdepart = this.time.addEvent({delay: 3000, callback: () =>{
+
     	text_depart1.setVisible(false);
     	this.physics.resume();
+
     }, 
+
     loop: false
+
 });
+
+
 
     // chrono toute la partie // 
 
+
     timer = this.time.addEvent({delay: 13000, callback: () =>{
+
     	victoire += 1;
     	this.physics.pause();
     	this.add.text(960, 540, 'Fin de la partie', {fontFamily: 'Treasuremap', fontSize: 130, color: '#000000'}).setDepth(10);
     },
+
     loop: false
+
 });
+
 }
+
 
 	update(){
 
+
 		// configuration des touches //
 
+
 		if (cursors.left.isDown && cursors.right.isUp){
+
 			player.anims.play('left', true);
 			player.setFlipX(false);
 			player.setVelocityX(-120);
 			player.setVelocityY(0);
+
 		}
+
 		else if (cursors.right.isDown && cursors.left.isUp){
+
 			player.anims.play('left', true);
 			player.setFlipX(true);
 			player.setVelocityX(120);
 			player.setVelocityY(0);
+
 		}
+
 		else if (cursors.right.isUp && cursors.left.isUp){
+
 			player.anims.play('stop', true);
 			player.setFlipX(false);
 			player.setVelocityX(0);
 			player.setVelocityY(0);
+
 		}
 }
 
@@ -141,68 +207,100 @@ class Jeu2 extends Phaser.Scene {
 
 		//fonctions //
 
+
 	function hitBateauEnm (player, bateauEnm){
+
 		nbrVie -= 1;
 		if (nbrVie == 2){vie3.disableBody(true, true);}
 		if (nbrVie == 1){vie2.disableBody(true, true);}
 		if (nbrVie == 0){
+
 			vie1.disableBody(true, true);
 			this.Physics.Pause();
 			this.add.text(960, 540, 'Perdu !', {fontFamily: 'Treasuremap', fontSize: 130, color: '#000000'}).setDepth(10);
+			this.add.text(960, 440, 'votre score est de : ' + score, {fontFamily: 'Treasuremap', fontSize: 130, color: '#000000'}).setDepth(10);
 		}
+		
 }
 
 	function hitIle (player,ile){
+
 		nbrVie -= 1;
 		if (nbrVie == 2){vie3.disableBody(true, true);}
 		if (nbrVie == 1){vie2.disableBody(true, true);}
 		if (nbrVie == 0){
+
 			vie1.disableBody(true, true);
 			this.Physics.Pause();
 			this.add.text(960, 540, 'Perdu !', {fontFamily: 'Treasuremap', fontSize: 130, color: '#000000'}).setDepth(10);
+			this.add.text(960, 440, 'votre score est de : ' + score, {fontFamily: 'Treasuremap', fontSize: 130, color: '#000000'}).setDepth(10);
 		}
+
 }
 
 	function hitIle2 (player, ile2){
+
 		nbrVie -= 1;
 		if (nbrVie == 2){vie3.disableBody(true, true);}
 		if (nbrVie == 1){vie2.disableBody(true, true);}
 		if (nbrVie == 0){
+
 			vie1.disableBody(true, true);
 			this.Physics.Pause();
 			this.add.text(960, 540, 'Perdu !', {fontFamily: 'Treasuremap', fontSize: 130, color: '#000000'}).setDepth(10);
+			this.add.text(960, 440, 'votre score est de : ' + score, {fontFamily: 'Treasuremap', fontSize: 130, color: '#000000'}).setDepth(10);
 		}
+
 }
 
 	function hitRochile (player, rochile){
+
 		nbrVie -= 1;
 		if (nbrVie == 2){vie3.disableBody(true, true);}
 		if (nbrVie == 1){vie2.disableBody(true, true);}
 		if (nbrVie == 0){
+
 			vie1.disableBody(true, true);
 			this.Physics.Pause();
 			this.add.text(960, 540, 'Perdu !', {fontFamily: 'Treasuremap', fontSize: 130, color: '#000000'}).setDepth(10);
+			this.add.text(960, 440, 'votre score est de : ' + score, {fontFamily: 'Treasuremap', fontSize: 130, color: '#000000'}).setDepth(10);
 		}
+
 }
 
 	function ExitWorld (bateauEnm, ile, ile2, rochile){
+
 	bateauEnm.disableBody();
 	ile.disableBody();	
 	ile2.disableBody();
 	rochile.disableBody();
+
 }
 
 
 	function fin (){
+
 		if (victoire == 1){
+
 			this.add.text(960, 540, 'Veuillez patienter', {fontFamily: 'Treasuremap', fontSize: 130, color: '#000000'}).setDepth(10);
+
+			if(nbrVie == 3){score += 300;}
+			if(nbrVie == 2){score += 200;}
+			if(nbrVie == 1){score += 100;}
+
 			scoreG += score;
+
 			this.time.addEvent({delay: 5000, callback: () =>{
+
 				MusicBar.mute = true;
 				this.scene.start('Jeu3');
+
 			},
+
 		loop: false
+
 			});
 
 		};
+
 	}
